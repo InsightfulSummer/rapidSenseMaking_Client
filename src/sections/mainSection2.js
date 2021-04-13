c5bbee1b78fba6816f12394b0f9f6d784a5b5429
amir23reza <amir.reza.haghverdi.khu@gmail.com>
2021-04-13 08:57:30 -0400

    13/4

--- src/sections/mainSection2.js
+++ src/sections/mainSection2.js
@@ -5,11 +5,12 @@ import {SetDimensions, setSliderHeightPorportion, SetZ, setZ} from '../redux/act
 
 const MainSection = () => {
 
-    const {width, height, slideHeightPorportion, z} = useSelector(state => ({
+    const {width, height, slideHeightPorportion, z, documents} = useSelector(state => ({
         width : state.canvasReducer.width,
         height : state.canvasReducer.height,
         slideHeightPorportion : state.canvasReducer.slideHeightPorportion,
-        z : state.canvasReducer.z
+        z : state.canvasReducer.z,
+        documents : state.dataReducer.documents,
     }))
     
     const sliderDragHandler = d3.drag()
@@ -48,6 +49,30 @@ const MainSection = () => {
             .attr("class", "canvasSVG")
             .append("g")
                 .attr("class", "mainContainer")
+
+        // define your linear gradient here ...
+        var defs = d3.select(".mainContainer")
+            .append("defs")
+                
+        var slideBodyGradient = defs.append("linearGradient")
+            .attr("id", "slideBodyGradient")
+            .attr("gradientTransform", "rotate(90)")
+
+        slideBodyGradient.append("stop")
+            .attr("offset" , "0%")
+            .attr("stop-color" , "#e6e6e6")
+
+        slideBodyGradient.append("stop")
+            .attr("offset" , "20%")
+            .attr("stop-color" , "#fff")
+
+        slideBodyGradient.append("stop")
+            .attr("offset" , "80%")
+            .attr("stop-color" , "#fff")
+
+        slideBodyGradient.append("stop")
+            .attr("offset" , "100%")
+            .attr("stop-color" , "#e6e6e6")
     }
 
     const loadSlider = () => {
@@ -61,23 +86,27 @@ const MainSection = () => {
     const updateSlider = () => {
 
         var slider = d3.select(".slideContainer")
+        var slideBody = slider.selectAll(".slideBody")
+        var slideController = slider.selectAll(".slideController")
 
-        slider.selectAll(".slideController").remove()
-        slider.selectAll(".slideBody").remove()
+        slideBody.remove()
+        slideController.remove()
 
-        var slideBody = slider.append("rect")
-            .attr("width", width)
-            .attr("height" , height*slideHeightPorportion)
-            .attr("x", 0)
-            .attr("y", z*height)
-            .attr("fill", 'red')
-            .attr("class" , "slideBody")
+        slideBody = slider.append("rect")
+                .attr("width", width)
+                .attr("x", 0)
+                .attr("fill", "url('#slideBodyGradient')")
+                .attr("class" , "slideBody")
+                .merge(slideBody)
+                .attr("y", z*height)
+                .attr("height" , height*slideHeightPorportion)
 
-        var slideController = slider.append("rect")
+        slideController = slider.append("rect")
             .attr("width", width/30)
             .attr("x", 0)
             .attr("fill", "green")
             .attr("class", "slideController")
+            .merge(slideController)
             .attr("y", z*height)
             .attr("height", height*slideHeightPorportion)
 
@@ -94,17 +123,61 @@ const MainSection = () => {
         })
     }
 
+    const loadDocs = () => {
+        d3.select(".mainContainer")
+            .append("g")
+            .attr("class", "docsContainer")
+
+        updateDocs()
+    }
+
+    const updateDocs = () => {
+        // console.log("in update Docs")
+        var y = 1 - (z + slideHeightPorportion)
+        var n_x = Math.floor(Math.pow(slideHeightPorportion, 2)*documents.length) 
+        var n_z = Math.floor((z/(1-slideHeightPorportion))*(documents.length - n_x))
+        let t_x = Math.pow((1/slideHeightPorportion), 2) // thickness of each element within the sliding window
+        let t_z = (height - documents.length*10) / (t_x*n_x + (documents.length - n_x)) // thickness of each element outisde of the sliding window
+
+        var docsContainer = d3.select(".docsContainer")
+
+        var newElements = docsContainer.selectAll(".docElement") 
+            .data(documents)  
+
+        newElements.exit().remove()
+
+        newElements
+            .enter()
+            .append("rect")
+            .merge(newElements)
+            .transition()
+            .attr("x",width/22)
+            .attr("y",(item, index)=>{
+                return index < n_z ? (index)*(t_z+10) : index < n_z+n_x ? n_z*(t_z + 10) + (index - n_z)*(t_x*t_z + 10) : n_z*(t_z + 10) + n_x*(t_x*t_z + 10) + (index - n_z - n_x)*(t_z+10)
+            })
+            .attr("width" , width/5)
+            .attr("height" , (item , index ) => {
+                return index < n_z ? t_z : index < n_z + n_x ? t_x * t_z : t_z
+            })
+            .attr("fill", "blue")
+            .attr("class", "docElement")
+            
+    }
+
     useEffect(()=>{
         getDimensions();
         setTimeout(()=>{
             loadSVG();
             loadSlider();
+            loadDocs();
         } , 250)
     } , [width, height])
 
     useEffect(()=>{
         updateSlider();
-    } , [slideHeightPorportion])
+        // console.log("in use effect")
+        updateDocs();
+    } , [slideHeightPorportion, z])
 
     return (
         <div id="mainCanvas_2">
