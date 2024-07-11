a4fc7bb3bc7144089864bcfb718ee8fd701395c6
Amir Haghighati <haghighati.amir@gmail.com>
2024-07-11 21:33:44 +0000

    fix: bootup bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,6 @@ import rootReducer from './reducers/rootReducer'
 import {applyMiddleware, compose, createStore} from 'redux'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    )
+const store = createStore(rootReducer, compose())
 
 export default store
